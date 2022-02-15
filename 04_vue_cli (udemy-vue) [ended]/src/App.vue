57520914d1776470e7f38b25bb79cd3e907c3ba3
Thiago Ferrari <thiagofcjr@gmail.com>
2022-02-14 22:14:33 -0300

    vue.js study

---
+++ 04_vue_cli (udemy-vue) [ended]/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+	<div id="app" >
+		<h1>Contadores</h1>
+		<Contador v-for="c in 3" :key='c'/>
+	</div>
+</template>
+
+<script>
+	import Contador from './components/Contador.vue'
+
+	export default {
+		components: {
+			Contador
+		}
+	}
+</script>
+
+<style>
+
+</style>
